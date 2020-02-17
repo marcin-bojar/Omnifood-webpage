ebbf0b4026416121d7c824ffd82a3301eca033ca
marcin-bojar <marcin.bojar@interia.pl>
2020-02-17 13:24:01 +0100

    Sticky nav p2

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+   
+    new Waypoint({
+        element: document.getElementsByClassName('js--section-features'),
+        handler: function(direction) {
+            if (direction == "down") {
+                $('nav').addClass('sticky');
+            }else {
+                $('nav').removeClass('sticky');
+            }
+        }, 
+        
+        offset: '60px;'
+        
+    });
     
     
     
+ /* var waypoint = new Waypoint({
+  element: document.getElementById('new-operator'),
+  handler: function(direction) {
+    notify(this.id + ' hit')
+  }
+})
+
+*/
     
 });
